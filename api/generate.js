183837fd7c190d88f24cd1555a801638bf391019
Godfather-team <godfather.team66@gmail.com>
2026-02-07 00:52:48 +0300

    Create generate.js

---
+++ api/generate.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // 32 chars
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+    const iv = crypto.randomBytes(IV_LENGTH);
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+module.exports = (req, res) => {
+    const webhook = req.query.webhook;
+    if (!webhook) return res.status(400).json({error: 'webhook required'});
+    
+    const token = encrypt(webhook);
+    const proxyUrl = `https://${req.headers.host}/api/send/${token}`;
+    
+    res.json({
+        token: token,
+        proxy_url: proxyUrl,
+        usage: `webh = "${proxyUrl}"`
+    });
+};
